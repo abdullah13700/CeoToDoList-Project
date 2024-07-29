3ef346e13b16cca4451968af4f8edf2cde6c8246
Abdullah Khaled <Abdullah11545@hotmail.com>
2024-07-29 20:25:20 +1200

    mock

---
+++ CeoToDoList/UnitTest/Mocking/MockedLists.cs
@@ -0,0 +1,34 @@
+﻿using CeoToDoList.Models.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTest.Mocking
+{
+    public static class MockedLists
+    {
+        public static List<CeoList> GetMockedLists()
+        {
+            return new List<CeoList>
+            {
+                new CeoList
+                {
+                    Id = Guid.Parse("09f321b9-7a72-4301-a345-1f8a66b1d8d7"),
+                    Title = "List 1",
+                    Description = "Description for List 1"
+                },
+                new CeoList
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "List 2",
+                    Description = "Description for List 2"
+                },
+                new CeoList
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "List 3",
+                    Description = "Description for List 3"
+                }
+            };
+        }
+    }
+}
---
+++ CeoToDoList/UnitTest/Mocking/MockedTasks.cs
@@ -0,0 +1,40 @@
+﻿using CeoToDoList.Models.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTest.Mocking
+{
+    public static class MockedTasks
+    {
+        public static List<CeoTask> GetMockedTasks()
+        {
+            return new List<CeoTask>
+            {
+                new CeoTask
+                {
+                    Id = Guid.Parse("09f321b9-7a72-4301-a345-1f8a66b1d8d0"),
+                    Title = "Duplicated Title",
+                    Description = "Description for Task 1",
+                    Completed = false,
+                    ListId = Guid.Parse("09f321b9-7a72-4301-a345-1f8a66b1d8d7")
+                },
+                new CeoTask
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Task 2",
+                    Description = "Description for Task 2",
+                    Completed = false,
+                    ListId = Guid.Parse("09f321b9-7a72-4301-a345-1f8a66b1d8d7")
+                },
+                new CeoTask
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Task 3",
+                    Description = "Description for Task 3",
+                    Completed = false,
+                    ListId = Guid.Parse("09f321b9-7a72-4301-a345-1f8a66b1d8d7")
+                }
+            };
+        }
+    }
+}
