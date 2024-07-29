3c2652eee02b513eedc6f1b805835341126f9879
Abdullah Khaled <Abdullah11545@hotmail.com>
2024-07-29 20:11:00 +1200

    added controller

---
+++ CeoToDoList/CeoToDoList/Controllers/ListController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using CeoToDoList.Models.Domain;
+using CeoToDoList.Models.DTO;
+using CeoToDoList.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CeoToDoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListController : ControllerBase
+    {
+        private readonly IListRepositories listRepositories;
+        private readonly IMapper mapper;
+
+        public ListController(IListRepositories listRepositories, IMapper mapper)
+        {
+            this.listRepositories = listRepositories;
+            this.mapper = mapper;
+        }
+
+        // Create list
+        // POST: /api/lists
+
+        [HttpPost]
+        public async Task<IActionResult> CreateList([FromBody] AddListReqDto addListReqDto)
+        {
+            // Map DTO to domain 
+            var listDomain = mapper.Map<CeoList>(addListReqDto);
+
+            await listRepositories.CreateAsync(listDomain);
+
+            return Ok(mapper.Map<AddListReqDto>(listDomain));
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllLists()
+        {
+            var result = await listRepositories.GetAllAsync();
+            return Ok(mapper.Map<List<CeoListDto>>(result));
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetListById([FromRoute] Guid id)
+        {
+            var listDomain = await listRepositories.GetByIdAsync(id);
+
+            return Ok(mapper.Map<CeoListDto>(listDomain));
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var listDomain = await listRepositories.DeleteAsync(id);
+            return Ok(mapper.Map<CeoListDto>(listDomain));
+        }
+
+    }
+}
---
+++ CeoToDoList/CeoToDoList/Controllers/TaskController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using CeoToDoList.Models.Domain;
+using CeoToDoList.Models.DTO;
+using CeoToDoList.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CeoToDoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly ITaskRepositories taskRepositories;
+
+        public TaskController(IMapper mapper, ITaskRepositories taskRepositories )
+        {
+            this.mapper = mapper;
+            this.taskRepositories = taskRepositories;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTask([FromBody] AddTaskReqDto addTaskReqDto ) 
+        {
+            var taskDomain = mapper.Map<CeoTask>(addTaskReqDto);
+
+            await taskRepositories.CreateAsync(taskDomain);
+
+            return Ok(mapper.Map<CeoTaskDto>(taskDomain));
+        
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteTask([FromRoute] Guid id)
+        {
+            var taskDomain = await taskRepositories.DeleteAsync(id);
+
+            return Ok(mapper.Map<CeoTaskDto>(taskDomain));
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateTask([FromRoute] Guid id, [FromBody] UpdateTaskReqDto updateTaskReqDto)
+        {
+            var taskDomain = mapper.Map<CeoTask>(updateTaskReqDto);
+
+            taskDomain = await taskRepositories.UpdateAsync(id, taskDomain);
+
+            return Ok(mapper.Map<CeoTaskDto>(taskDomain));
+        }
+
+        [HttpPut]
+        [Route("complete/{id:guid}")]
+        public async Task<IActionResult> UpdateTaskCompleted([FromRoute] Guid id, [FromBody] UpdateCompleteTaskDto updateCompleteTaskDto)
+        {
+            var taskDomain = mapper.Map<CeoTask>(updateCompleteTaskDto);
+
+            taskDomain = await taskRepositories.UpdateCompletedAsync(id, taskDomain);
+
+            return Ok(mapper.Map<CeoTaskDto>(taskDomain));
+        }
+
+    }
+}
