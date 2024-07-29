95ad5cd37188a9e01d82c1f5618ae5ec2d027159
Abdullah Khaled <Abdullah11545@hotmail.com>
2024-07-29 20:24:04 +1200

    rest

---
+++ CeoToDoList/CeoToDoList/Program.cs
@@ -0,0 +1,43 @@
+using CeoToDoList.Data;
+using CeoToDoList.Repositories;
+using Microsoft.EntityFrameworkCore;
+using CeoToDoList.Mappings;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Connecting to the database
+builder.Services.AddDbContext<CeoDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("CeoConnectionString")));
+
+// repo inject
+builder.Services.AddScoped<IListRepositories, SQLListRepository>();
+builder.Services.AddScoped<ITaskRepositories, SQLTaskRepository>();
+
+// mapper inject
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
