c14776e5e4e6a9c3637d6195a2da1626e37c4c18
Abdullah Khaled <Abdullah11545@hotmail.com>
2024-07-29 20:12:56 +1200

    Add data folder

---
+++ CeoToDoList/CeoToDoList/Data/CeoDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CeoToDoList.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CeoToDoList.Data
+{
+    public class CeoDbContext : DbContext
+    {
+        public CeoDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        public DbSet<CeoList> Lists { get; set; }
+        public DbSet<CeoTask> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure one-to-many relationship
+            modelBuilder.Entity<CeoList>()
+                .HasMany(cl => cl.Tasks)
+                .WithOne(ct => ct.CeoList)
+                .HasForeignKey(ct => ct.ListId);
+      
+        }
+    }
+}
---
+++ CeoToDoList/CeoToDoList/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CeoToDoList.Models.Domain;
+using CeoToDoList.Models.DTO;
+
+namespace CeoToDoList.Mappings
+{
+    public class AutoMapperProfiles:Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AddListReqDto,CeoList>().ReverseMap();
+            CreateMap<AddTaskReqDto,CeoTask>().ReverseMap();
+            CreateMap<CeoListDto, CeoList>().ReverseMap();
+            CreateMap<CeoTaskDto, CeoTask>().ReverseMap();
+            CreateMap<UpdateTaskReqDto, CeoTask>().ReverseMap();
+            CreateMap<UpdateCompleteTaskDto, CeoTask>().ReverseMap();
+
+        }
+    }
+}
